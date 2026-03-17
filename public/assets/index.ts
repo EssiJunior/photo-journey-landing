980a12b5a5d074bea20c51c4a76997c67f3f0255
EssiJunior <nessipjunior@gmail.com>
2026-03-17 21:25:29 +0100

    Imported all images to app

---
+++ public/assets/index.ts
@@ -0,0 +1,11 @@
+import thumb1 from "@/assets/thumb1.webp";
+import thumb2 from "@/assets/thumb2.webp";
+import thumb3 from "@/assets/thumb3.webp";
+import thumb4 from "@/assets/thumb4.webp";
+import thumb5 from "@/assets/thumb5.webp";
+import thumb6 from "@/assets/thumb6.webp";
+import thumb7 from "@/assets/thumb7.webp";
+import thumb8 from "@/assets/thumb8.webp";
+import thumb9 from "@/assets/thumb9.webp";
+
+export {thumb1, thumb2, thumb3, thumb4, thumb5, thumb6, thumb7, thumb8, thumb9 }
