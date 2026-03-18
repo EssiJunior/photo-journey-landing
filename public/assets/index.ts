569f33406b004ea526007ae149e2521a2717e921
EssiJunior <nessipjunior@gmail.com>
2026-03-18 07:59:43 +0100

    Configured assets imports

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,11 +1,41 @@
-import thumb1 from "@/assets/thumb1.webp";
-import thumb2 from "@/assets/thumb2.webp";
-import thumb3 from "@/assets/thumb3.webp";
-import thumb4 from "@/assets/thumb4.webp";
-import thumb5 from "@/assets/thumb5.webp";
-import thumb6 from "@/assets/thumb6.webp";
-import thumb7 from "@/assets/thumb7.webp";
-import thumb8 from "@/assets/thumb8.webp";
-import thumb9 from "@/assets/thumb9.webp";
+// -- ICONS
+import adventure from "@/assets/icons/adventure.svg";
+import arrowBack from "@/assets/icons/back-arrow.svg";
+import chevronLeft from "@/assets/icons/chevron-left.svg";
+import chevronRight from "@/assets/icons/chevron-right.svg";
+import elephant from "@/assets/icons/elephant.svg";
+import share from "@/assets/icons/share.svg";
+import solo from "@/assets/icons/solo.svg";
 
-export {thumb1, thumb2, thumb3, thumb4, thumb5, thumb6, thumb7, thumb8, thumb9 }
+// - social
+import facebookIcon from "@/assets/icons/social/facebook.svg";
+import instagramIcon from "@/assets/icons/social/instagram.svg";
+import linkedinIcon from "@/assets/icons/social/linkedin.svg";
+import tiktokIcon from "@/assets/icons/social/tiktok.svg";
+
+// - stores
+import appstore from "@/assets/images/stores/appstore.webp";
+import playstore from "@/assets/images/stores/playstore.webp";
+
+// -- IMAGES
+import profile from "@/assets/images/profile.webp";
+
+// - language
+import english from "@/assets/language/english.webp";
+
+// - social
+import instagram from "@/assets/images/social/instagram.webp";
+import youtube from "@/assets/images/social/youtube.webp";
+
+// - thumbs
+import thumb1 from "@/assets/images/thumbs/1.webp";
+import thumb2 from "@/assets/images/thumbs/2.webp";
+import thumb3 from "@/assets/images/thumbs/3.webp";
+import thumb4 from "@/assets/images/thumbs/4.webp";
+import thumb5 from "@/assets/images/thumbs/5.webp";
+import thumb6 from "@/assets/images/thumbs/6.webp";
+import thumb7 from "@/assets/images/thumbs/7.webp";
+import thumb8 from "@/assets/images/thumbs/8.webp";
+import thumb9 from "@/assets/images/thumbs/9.webp";
+
+export { adventure, arrowBack, chevronLeft, chevronRight, elephant, share, solo, facebookIcon, instagramIcon, linkedinIcon, tiktokIcon, appstore, playstore, english, instagram, youtube, profile, thumb1, thumb2, thumb3, thumb4, thumb5, thumb6, thumb7, thumb8, thumb9 }
