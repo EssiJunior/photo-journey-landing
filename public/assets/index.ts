c8eb64a3c5a7c3d9aa84a0460fbe300def8611bd
EssiJunior <nessipjunior@gmail.com>
2026-03-22 00:24:57 +0100

    Corrected assests import error

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,41 +1,41 @@
 // -- ICONS
-import adventure from "@/assets/icons/adventure.svg";
-import arrowBack from "@/assets/icons/back-arrow.svg";
-import chevronLeft from "@/assets/icons/chevron-left.svg";
-import chevronRight from "@/assets/icons/chevron-right.svg";
-import elephant from "@/assets/icons/elephant.svg";
-import share from "@/assets/icons/share.svg";
-import solo from "@/assets/icons/solo.svg";
+import adventure from "./icons/adventure.svg";
+import arrowBack from "./icons/back-arrow.svg";
+import chevronLeft from "./icons/chevron-left.svg";
+import chevronRight from "./icons/chevron-right.svg";
+import elephant from "./icons/elephant.svg";
+import share from "./icons/share.svg";
+import solo from "./icons/solo.svg";
 
 // - social
-import facebookIcon from "@/assets/icons/social/facebook.svg";
-import instagramIcon from "@/assets/icons/social/instagram.svg";
-import linkedinIcon from "@/assets/icons/social/linkedin.svg";
-import tiktokIcon from "@/assets/icons/social/tiktok.svg";
+import facebookIcon from "./icons/social/facebook.svg";
+import instagramIcon from "./icons/social/instagram.svg";
+import linkedinIcon from "./icons/social/linkedin.svg";
+import tiktokIcon from "./icons/social/tiktok.svg";
 
 // - stores
-import appstore from "@/assets/images/stores/appstore.webp";
-import playstore from "@/assets/images/stores/playstore.webp";
+import appstore from "./icons/stores/appstore.svg";
+import playstore from "./icons/stores/playstore.svg";
 
 // -- IMAGES
-import profile from "@/assets/images/profile.webp";
+import profile from "./images/profile.webp";
 
 // - language
-import english from "@/assets/language/english.webp";
+import english from "./images/language/english.webp";
 
 // - social
-import instagram from "@/assets/images/social/instagram.webp";
-import youtube from "@/assets/images/social/youtube.webp";
+import instagram from "./images/social/instagram.webp";
+import youtube from "./images/social/youtube.webp";
 
 // - thumbs
-import thumb1 from "@/assets/images/thumbs/1.webp";
-import thumb2 from "@/assets/images/thumbs/2.webp";
-import thumb3 from "@/assets/images/thumbs/3.webp";
-import thumb4 from "@/assets/images/thumbs/4.webp";
-import thumb5 from "@/assets/images/thumbs/5.webp";
-import thumb6 from "@/assets/images/thumbs/6.webp";
-import thumb7 from "@/assets/images/thumbs/7.webp";
-import thumb8 from "@/assets/images/thumbs/8.webp";
-import thumb9 from "@/assets/images/thumbs/9.webp";
+import thumb1 from "./images/thumbs/1.webp";
+import thumb2 from "./images/thumbs/2.webp";
+import thumb3 from "./images/thumbs/3.webp";
+import thumb4 from "./images/thumbs/4.webp";
+import thumb5 from "./images/thumbs/5.webp";
+import thumb6 from "./images/thumbs/6.webp";
+import thumb7 from "./images/thumbs/7.webp";
+import thumb8 from "./images/thumbs/8.webp";
+import thumb9 from "./images/thumbs/9.webp";
 
 export { adventure, arrowBack, chevronLeft, chevronRight, elephant, share, solo, facebookIcon, instagramIcon, linkedinIcon, tiktokIcon, appstore, playstore, english, instagram, youtube, profile, thumb1, thumb2, thumb3, thumb4, thumb5, thumb6, thumb7, thumb8, thumb9 }
