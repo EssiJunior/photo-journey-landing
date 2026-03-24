775e0efbed6a3e60bb1df60af08103087a76b90a
EssiJunior <nessipjunior@gmail.com>
2026-03-24 12:10:34 +0100

    Fixed issue on Copyrighted Image

--- components/atoms/ImageCopyrighted.tsx
+++ components/atoms/ImageCopyrighted.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const ImageCopyrighted = ({ children, className, copyright }: { children: React.ReactNode, className?: string, copyright: string }) => {
     return (
-        <div className={`bg-red-400 relative ${className}`}>
+        <div className={`relative ${className}`}>
             {children}
-            <p className='absolute bottom-7.25 left-1/2 -translate-x-1/2 py-1 px-2 bg-black text-white'>{copyright}</p>
+            <p className='absolute bottom-7.25 left-1/2 -translate-x-1/2 py-1 px-2 bg-black/50 text-white font-medium leading-[20px] text-[clamp(14px,1vw,16px)]'>{copyright}</p>
         </div>
     )
 }
